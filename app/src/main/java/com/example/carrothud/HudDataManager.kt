161bc907ba8790aba9450fac9cbff33a9cb5e902
lm2290-alt <lm2290@naver.com>
2026-09-16 00:50:21 +0900

    Update HudDataManager.kt

--- app/src/main/java/com/example/carrothud/HudDataManager.kt
+++ app/src/main/java/com/example/carrothud/HudDataManager.kt
@@ -1,7 +1,9 @@
 package com.example.carrothud
 
+import android.app.Activity
 import android.webkit.WebView
 
 object HudDataManager {
     var webView: WebView? = null
+    var activity: Activity? = null
 }

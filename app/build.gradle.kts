6e5c85c370c0f64d511fadd774382e2d19b18609
lm2290-alt <lm2290@naver.com>
2026-09-17 21:29:40 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("androidx.car.app:app:1.4.0")
-    implementation("androidx.car.app:app-projected:1.4.0")
+    implementation("androidx.car.app:app:1.7.0")
+    implementation("androidx.car.app:app-projected:1.7.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
